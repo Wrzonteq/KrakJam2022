424c7f1e0d124b02fb7c82aa80afd3d0e721b074
mswiszcz <mateusz@swiszcz.pl>
2022-01-29 20:36:50 +0100

    Wip shooting

--- KrakJam2022 - Unity/Assets/_Code/Gameplay/PlayerController.cs
+++ KrakJam2022 - Unity/Assets/_Code/Gameplay/PlayerController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using PartTimeKamikaze.KrakJam2022.Combat;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,16 +8,33 @@ namespace PartTimeKamikaze.KrakJam2022 {
         [SerializeField] Rigidbody2D selfRigidbody2D;
         [SerializeField] int movementSpeed = 10;
         [SerializeField] Transform crosshairFollowTarget;
+        [SerializeField] Bullet bulletPrefab;
 
-        List<IInteractable> InteractablesInRange { get; } = new List<IInteractable>();
+        List<IInteractable> InteractablesInRange { get; } = new();
 
+        
+        [SerializeField] float bulletSpeed = 6.66f;
+        [SerializeField] float shootEveryInSec = 0.5f;
+
+        float nextShotIn = 0;
+        
         Vector3 move;
+
+        bool isShooting = false; // Range?
 
         public Transform CrosshairFollowTarget => crosshairFollowTarget;
 
-
         void Update() {
             selfRigidbody2D.velocity = move * movementSpeed;
+
+            if (isShooting && nextShotIn <= 0) { // && GameSystems.GetSystem<GameStateSystem>().runtimeGameState.stage == GameStage.Insanity) {
+                Shoot();
+                nextShotIn = shootEveryInSec;
+            }
+
+            if (nextShotIn > 0) {
+                nextShotIn -= Time.deltaTime;
+            }
         }
 
         public void OnMove(InputValue value) {
@@ -27,6 +45,17 @@ namespace PartTimeKamikaze.KrakJam2022 {
             if (value.isPressed && InteractablesInRange.Count > 0) {
                 InteractablesInRange[0].Interact();
             }
+        }
+
+        public void OnFire(InputValue value) {
+            isShooting = value.isPressed;
+        }
+
+        public void Shoot() {
+            Bullet bullet = Instantiate(bulletPrefab);
+            bullet.transform.position = transform.position;
+
+            bullet.Fire(GameSystems.GetSystem<CameraSystem>().CrosshairInstance.transform.localPosition, bulletSpeed);
         }
 
         public void RegisterInteractable(IInteractable interactable) {
