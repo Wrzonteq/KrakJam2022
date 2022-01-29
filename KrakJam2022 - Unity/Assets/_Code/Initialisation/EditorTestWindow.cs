fb8567a26317c0934a80011341904376b81cda21
Bartosz Stoliński <barf@krej.net>
2022-01-29 17:47:38 +0100

    Test window

---
+++ KrakJam2022 - Unity/Assets/_Code/Initialisation/EditorTestWindow.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+namespace PartTimeKamikaze.KrakJam2022 {
+    public class TestingWindow : EditorWindow {
+
+
+        [MenuItem("KrakJam2022/Test Window", false, 0)]
+        static void OpenTheWindow() {
+            GetWindow<TestingWindow>("HF2 Toolbox");
+        }
+
+        void OnGUI() {
+            if (!Application.isPlaying) {
+                GUILayout.Label("Enter playmode to see tests.");
+            } else
+                DrawWindow();
+        }
+
+        void DrawWindow() {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Start enemies spawning")) {
+                GameSystems.GetSystem<EnemiesSystem>().StartEnemySpawning().Forget();
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
+
+#endif
