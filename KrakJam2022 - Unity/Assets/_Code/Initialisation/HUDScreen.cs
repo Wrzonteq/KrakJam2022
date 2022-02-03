d0aff27897afc13f45e5b52e08bd39c486078e9e
LongBearded <jakub.stadtmuller@gmail.com>
2022-02-03 21:21:27 +0100

    Remove unnecessary methods

--- KrakJam2022 - Unity/Assets/_Code/Initialisation/HUDScreen.cs
+++ KrakJam2022 - Unity/Assets/_Code/Initialisation/HUDScreen.cs
@@ -14,12 +14,6 @@ namespace PartTimeKamikaze.KrakJam2022.UI {
             insanityDisplay.gameObject.SetActive(false);
         }
 
-        void OnDestroy() {
-            var gameStateSystem = GameSystems.GetSystem<GameStateSystem>();
-            gameStateSystem.Insanity.ChangedValue -= SetProgress;
-            gameStateSystem.Stage.ChangedValue -= HandleStageChanged;
-        }
-
         void HandleStageChanged(GameStage stage) {
             insanityDisplay.gameObject.SetActive(stage == GameStage.Insanity);
         }
--- KrakJam2022 - Unity/Assets/_Code/Initialisation/InsanitySystem.cs
+++ KrakJam2022 - Unity/Assets/_Code/Initialisation/InsanitySystem.cs
@@ -15,10 +15,6 @@ namespace PartTimeKamikaze.KrakJam2022 {
             GameSystems.GetSystem<GameStateSystem>().Stage.ChangedValue += HandleStageChanged;
         }
 
-        void OnDestroy() {
-            GameSystems.GetSystem<GameStateSystem>().Stage.ChangedValue -= HandleStageChanged;
-        }
-
         void HandleStageChanged(GameStage stage) {
             if(stage == GameStage.Insanity)
                 InitInsanity();
